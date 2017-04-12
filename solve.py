583c12ecdca5f25b8bd64d0f7ff9f1f153e65a32
Mathias Bjerke <mattiz@online.no>
2017-04-12 17:18:11 +0200

    Added examples for parsing map, and pathfinding

--- solve.py
+++ solve.py
@@ -3,22 +3,21 @@ import sys
 import random
 import json
 
- 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-    print 'Failed to create socket'
-    sys.exit()
- 
-host = '127.0.0.1';
-port = 54321;
+
+s = socket.socket()
+host = socket.gethostname()
+port = 54321
  
 s.connect((host, port))
 
+s.send(b"NAME MooG\n")
+
 while True: 
 	reply = s.recv(4096)
-	print json.dumps( reply, sort_keys=True, indent=4, separators=(',', ': '))
+	#print( json.dumps( reply, sort_keys=True, indent=4, separators=(',', ': ')))
 
-	moves = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+	print(reply)
+
+	moves = [b'UP', b'DOWN', b'LEFT', b'RIGHT']
 	move = moves[random.randint(0, 3)]
-	s.sendall( move )
+	s.send( move )
