93dec3d592093feae73680aadac781e1d9a2c93b
Mathias Bjerke <mattiz@online.no>
2017-04-13 13:42:42 +0200

    Refactoring

--- run.py
+++ run.py
@@ -3,6 +3,33 @@ import json
 from World import World
 
 
+#
+# Handle game state
+#
+def handle_gamestate():
+	w = World(data)
+	# Adding targets
+	targets = []
+	if w.me['isdangerous']:
+		for p in w.pellets:
+			targets.append(p)
+	else:
+		for sp in w.superpellets:
+			targets.append(sp)
+
+	# Consuming targets
+	if len(targets) > 0:
+		target = targets.pop()
+		move = w.getnextmove(target['x'], target['y'])
+
+	# Make move
+	if move:
+		s.send(move)
+
+
+#
+# Socket, socket, ten dollah!
+#
 s = socket.socket()
 host = socket.gethostname()
 port = 54321
@@ -11,7 +38,11 @@ s.connect((host, port))
 
 s.send(b"NAME MooG\n")
 
-while True: 
+
+#
+# Main loop
+#
+while True:
 	reply = s.recv(4096).decode('ascii')
 	parts = reply.strip().split("\n")
 
@@ -19,23 +50,4 @@ while True:
 		data = json.loads( part )
 
 		if 'gamestate' in data:
-			w = World( data )
-
-			# Adding targets
-			targets = []
-
-			if w.me['isdangerous']:
-				for p in w.pellets:
-					targets.append(p)
-			else:
-				for sp in w.superpellets:
-					targets.append(sp)
-
-			# Consuming targets
-			if len(targets) > 0:
-				target = targets.pop()
-				move = w.getnextmove( target['x'], target['y'] )
-
-			# Make move
-			if move:
-				s.send( move )
+			handle_gamestate()
